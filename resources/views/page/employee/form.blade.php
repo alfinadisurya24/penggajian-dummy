afd3d76a659d3e8aa0256030f6f05e2aad2775ce
alfinadisurya24 <alfinadisurya24@gmail.com>
2025-05-13 17:14:08 +0700

    update: fix label option

--- resources/views/page/employee/form.blade.php
+++ resources/views/page/employee/form.blade.php
@@ -48,7 +48,7 @@
                     <label for="gender" class="block mb-2 text-sm font-medium">Jenis Kelamin</label>
                     <select class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" name="gender" id="gender">
                         <option value="laki-laki" {{ ($action == 'update' ? $data->gender == 'laki-laki' : old('gender') == 'laki-laki') ? 'selected' : '' }}>Laki-laki</option>
-                        <option value="perempuan" {{ ($action == 'update' ? $data->gender == 'perempuan' : old('gender') == 'perempuan') ? 'selected' : '' }}>Perempuan"></option>
+                        <option value="perempuan" {{ ($action == 'update' ? $data->gender == 'perempuan' : old('gender') == 'perempuan') ? 'selected' : '' }}>Perempuan</option>
                     </select>
                 </div>
                 <div>
